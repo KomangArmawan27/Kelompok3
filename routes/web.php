2437c49d1299c05f55b98705e992d48ec0c4bf6c
Indra Oki Sandy <indraokisandy1@gmail.com>
2021-11-08 22:18:14 +0700

    Perbarui tampilan

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.blade.php');
+Route::get('/', static function () {
+    return view('admin');
 });
