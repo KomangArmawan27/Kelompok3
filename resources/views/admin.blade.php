60dd4894ccff5b23200b2a9dcf15ec816004b6d5
Komang Armawan <komang.armawan.27@gmail.com>
2021-11-11 22:07:29 +0700

    logo

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -29,8 +29,8 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                <div class="sidebar-brand-icon ">
+                    <img src="/asset/img/LOGO KOPMA.png">
                 </div>
                 <div class="sidebar-brand-text mx-3">KOPERASI MAHASISWA</div>
             </a>
