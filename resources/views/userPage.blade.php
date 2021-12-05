482e27ba10623bd3fac0d73b7e2b8aac897e2a95
Komang Armawan <komang.armawan.27@gmail.com>
2021-12-05 14:34:14 +0700

    login button

--- resources/views/userPage.blade.php
+++ resources/views/userPage.blade.php
@@ -52,7 +52,7 @@
                     <li><a class="nav-link scrollto" href="#about">Tentang Kopma</a></li>
                     <li><a class="nav-link scrollto" href="#services">Jasa</a></li>
                     <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-                    <li><a class="getstarted scrollto" href="#">Login</a></li>
+                    <li><a class="getstarted scrollto" href="login ">Login</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
