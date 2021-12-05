3472547d8a978fccc4b2b5182920f32c33b51cbb
Indra Oki Sandy <81407568+rezaoktaviansandyxx@users.noreply.github.com>
2021-12-05 14:33:02 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,34 +1,34 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
 use Validator,Redirect,Response;
 Use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Session;
- 
+
 class UserController extends Controller
 {
- 
+
     public function index()
     {
         return view('login');
     }  
- 
+
     public function registration()
     {
         return view('register');
     }
-     
+    
     public function postLogin(Request $request)
     {
         request()->validate([
         'email' => 'required',
         'password' => 'required|min:5',
         ]);
- 
+
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
             // Authentication passed...
@@ -36,7 +36,7 @@ class UserController extends Controller
         }
         return Redirect::to("login")->withSuccess('Oppes! You have entered invalid credentials');
     }
- 
+
     public function postRegistration(Request $request)
     {  
         request()->validate([
@@ -44,67 +44,20 @@ class UserController extends Controller
         'email' => 'required|email|unique:users',
         'password' => 'required|min:5',
         ]);
-         
+
         $data = $request->all();
- 
+
         $check = $this->create($data);
-       
+
         return Redirect::to("login")->withSuccess('Great! You have Successfully Register');
     }
- 
+
     public function create(array $data)
     {
-      return User::create([
+    return User::create([
         'name' => $data['name'],
         'email' => $data['email'],
         'password' => Hash::make($data['password']),
-      ]);
-    }
-     
-    public function logout() {
-        Session::flush();
-        Auth::logout();
-        return Redirect('login');
-    }
-    public function contact() {
-    /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-    $receiving_email_address = 'contact@example.com';
-
-    if( file_exists($php_email_form = '/asset/vendor/php-email-form/php-email-form.php' )) {
-        include( $php_email_form );
-    } else {
-        die( 'Unable to load the "PHP Email Form" Library!');
-    }
-
-    $contact = new PHP_Email_Form;
-    $contact->ajax = true;
-    
-    $contact->to = $receiving_email_address;
-    $contact->from_name = $_POST['name'];
-    $contact->from_email = $_POST['email'];
-    $contact->subject = $_POST['subject'];
-
-    // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-    /*
-    $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-    );
-  */
-
-    $contact->add_message( $_POST['name'], 'From');
-    $contact->add_message( $_POST['email'], 'Email');
-    $contact->add_message( $_POST['message'], 'Message', 10);
-
-    echo $contact->send();
+    ]);
     }
 }
