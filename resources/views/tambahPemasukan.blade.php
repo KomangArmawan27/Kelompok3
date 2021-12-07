ea6355760e082739b54172bbd202ea6bb85baa23
Indra Oki Sandy <81407568+rezaoktaviansandyxx@users.noreply.github.com>
2021-12-07 21:00:11 +0700

    Admin dan User (frontend)

--- resources/views/tambahPemasukan.blade.php
+++ resources/views/tambahPemasukan.blade.php
@@ -28,7 +28,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard">
                 <div class="sidebar-brand-icon ">
                     <img src="/asset/img/LOGO KOPMA.png">
                 </div>
@@ -40,7 +40,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="dashboard">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -180,7 +180,7 @@
                         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                         <div class="modal-footer">
                             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                            <a class="btn btn-primary" href="login.html">Logout</a>
+                            <a class="btn btn-primary" href="logout">Logout</a>
                         </div>
                     </div>
                 </div>
